226c36b794ff0126101061448c0d055aa116afa0
Ledexsoft <ledexsoftrgmail.com>
2024-03-21 09:12:15 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   Booking/assets/js/formulario_de_citas.js
    	modified:   Booking/booking.html
    	modified:   firebaseconect.js

--- Booking/assets/js/formulario_de_citas.js
+++ Booking/assets/js/formulario_de_citas.js
@@ -1,1 +1,12 @@
 const ReservarCitaFormulario = document.getElementById("ReservarCitaFormulario");
+const BtnEnviarFormulario = document.getElementById(BtnEnviarFormulario)
+
+ReservarCitaFormulario.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const X = BtnEnviarFormulario.value;
+
+    await addDoc(colection(firestore, "2"), {
+        Y: X
+    });
+} 
+);
